a5f872c9d83aff4881b2485480ac4d9df09dc2b9
Pantheonn3D <pantheon3d.contact@gmail.com>
2025-11-13 14:43:16 +0100

    added codedump.txt for pasting files

--- src/main/java/com/ixd3grp2/frontend/MainApp.java
+++ src/main/java/com/ixd3grp2/frontend/MainApp.java
@@ -8,22 +8,23 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
+
     @Override
     public void start(Stage stage) {
-
+        
         StackPane MainScreen = new StackPane();
-        Rectangle square = new Rectangle(20,20,20,20);
+        Rectangle square = new Rectangle(20, 20, 20, 20);
         square.setFill(Color.GREEN);
         MainScreen.getChildren().add(square);
 
         Scene scene = new Scene(MainScreen, 600, 900);
 
         stage.setScene(scene);
-        stage.setTitle("TEST TIELE");
+        stage.setTitle("TEST TITLE");
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
