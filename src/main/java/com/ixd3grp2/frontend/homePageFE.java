1f35897491769cbfe28558bb920303f77eca41f2
PowerSupply-sudo <nicklasvinther@proton.me>
2025-11-21 12:04:32 +0100

    Updated homepage

--- src/main/java/com/ixd3grp2/frontend/homePageFE.java
+++ src/main/java/com/ixd3grp2/frontend/homePageFE.java
@@ -116,9 +116,9 @@ public class homePageFE extends Application{
         bottombar.getChildren().addAll(searchButton, homeButton, profileButton);
     
         // Use a BorderPane to position the bottombar at the bottom
-        /**BorderPane layout = new BorderPane();
-        layout.setCenter(mainContent);//// Main content in the center
-        layout.setBottom(bottombar); // Add the bottom bar to the bottom of the layout*/
+        BorderPane layout = new BorderPane();
+        layout.setCenter(centerContent);//// Main content in the center
+        layout.setBottom(bottombar); // Add the bottom bar to the bottom of the layout
        
 
         // We instantiate a new Scene of size 300x250, with white background and and associated scene graph rooted in 'layout'
